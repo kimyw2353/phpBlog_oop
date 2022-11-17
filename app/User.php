643268e800e47e6aab2d07cd993a4e666d39c5ca
kimyw2353 <kimyw2353@naver.com>
2022-11-17 16:50:31 +0900

    회원가입model 수정

--- app/User.php
+++ app/User.php
@@ -1,13 +1,20 @@
 <?php
+
 namespace App;
 
 use Eclair\Database\Adaptor;
 
 class User
 {
+	public function create()
+	{
+		$sql = "INSERT INTO users(`email`, `password`) VALUES (?,?)";
+		return Adaptor ::exec($sql, [$this -> email, $this -> password]);
+	}
+	
 	public function getUserName()
 	{
-		return current(explode('@', $this->email));
+		return current(explode('@', $this -> email));
 	}
-
+	
 }
