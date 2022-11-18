4866d426831bc9229a1e52cf76552682d94023ce
kimyw2353 <kimyw2353@naver.com>
2022-11-18 11:01:42 +0900

    route파일 require추가

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -9,7 +9,10 @@ class RouteServiceProvider extends ServiceProvider
 {
 	public static function register()
 	{
-		require_once dirname(__DIR__, 2).'/routes/web.php';
+		//require_once dirname(__DIR__, 2).'/routes/web.php';
+		foreach ([ 'web', 'api' ] as $name) {
+			require_once dirname(__DIR__,2). "/routes/".$name.".php";
+		}
 	}
 	
 	public static function boot()
