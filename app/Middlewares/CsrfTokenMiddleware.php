14ba5d89d9733d629bf237de37006b68e41a931d
kimyw2353 <kimyw2353@naver.com>
2022-11-18 09:45:19 +0900

    json_decode 오타수정

--- app/Middlewares/CsrfTokenMiddleware.php
+++ app/Middlewares/CsrfTokenMiddleware.php
@@ -6,12 +6,11 @@ use Eclair\Routing\Middleware;
 
 class CsrfTokenMiddleware extends Middleware{
 	
-	public static function process(): ?bool
+	public static function process()
 	{
-		$csrfToken = filter_input(INPUT_POST, '_csrfToken', FILTER_SANITIZE_STRING)
-			?: json_decode(file_get_contents('php://input'))->csrfToken;
+		$csrfToken = filter_input(INPUT_POST, '_csrfToken', FILTER_SANITIZE_STRING) ?: json_decode(file_get_contents('php://input'))->_csrfToken;
 		
-		if (\hash_equals($csrfToken, $_SESSION['CSRF_TOKEN'])){
+		if (hash_equals($csrfToken, $_SESSION['CSRF_TOKEN'])){
 			return true;
 		}
 		return header('Location: ' . $_SERVER['HTTP_REFERER']);
